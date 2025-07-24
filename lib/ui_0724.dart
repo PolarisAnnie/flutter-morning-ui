7d8bc9e3c2ad315649e5c42fb22d5a479ba3a9b9
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-07-24 09:48:39 +0900

    WIP: 휴대폰 전화 화면 작업 중

--- lib/ui_0724.dart
+++ lib/ui_0724.dart
@@ -3,6 +3,48 @@ import 'package:flutter/material.dart';
 class Home0724 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Container(width: double.infinity, height: 300),
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Center(
+                    child: Text('1', style: TextStyle(fontSize: 40)),
+                  ),
+                ),
+                SizedBox(width: 30),
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+                SizedBox(width: 30),
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
