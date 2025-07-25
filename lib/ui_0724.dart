777d6a1ba7da49e932d1f7efc9c72b05e102e084
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-07-25 10:03:25 +0900

    휴대폰 전화 화면 UI

--- lib/ui_0724.dart
+++ lib/ui_0724.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home0724 extends StatelessWidget {
@@ -5,46 +6,120 @@ class Home0724 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 50),
         child: Column(
           children: [
-            Container(width: double.infinity, height: 300),
+            SizedBox(height: MediaQuery.sizeOf(context).height * 0.4),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                numberCircle('1', ' ', null),
+                SizedBox(width: 20),
+                numberCircle('2', 'ABC', null),
+                SizedBox(width: 20),
+                numberCircle('3', 'DEF', null),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numberCircle('4', 'GHI', null),
+                SizedBox(width: 20),
+                numberCircle('5', 'JKL', null),
+                SizedBox(width: 20),
+                numberCircle('6', 'MNO', null),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numberCircle('7', 'PQRS', null),
+                SizedBox(width: 20),
+                numberCircle('8', 'TUV', null),
+                SizedBox(width: 20),
+                numberCircle('9', 'WXYZ', null),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numberCircle('*', '', null),
+                SizedBox(width: 20),
+                numberCircle('0', '+', null),
+                SizedBox(width: 20),
+                numberCircle('#', '', null),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                numberCircle('', '', Colors.transparent),
+                SizedBox(width: 25),
                 Container(
-                  width: 100,
-                  height: 100,
+                  width: 80,
+                  height: 80,
                   decoration: BoxDecoration(
-                    color: Colors.grey,
+                    color: Color.fromRGBO(101, 196, 102, 1),
                     borderRadius: BorderRadius.circular(100),
                   ),
-                  child: Center(
-                    child: Text('1', style: TextStyle(fontSize: 40)),
+                  child: Icon(
+                    CupertinoIcons.phone_fill,
+                    color: Colors.white,
+                    size: 25,
                   ),
                 ),
-                SizedBox(width: 30),
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                ),
-                SizedBox(width: 30),
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                ),
+                SizedBox(width: 20),
+                numberCircle('', '', Colors.transparent),
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Container numberCircle(String number, String alphabet, Color? color) {
+    return Container(
+      width: 80,
+      height: 80,
+      decoration: BoxDecoration(
+        color: color ?? Colors.grey[300],
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: numberText(number, alphabet),
+    );
+  }
+
+  Column numberText(String number, String alphabet) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          number,
+          style: TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.w500,
+            height: 1.0,
+          ),
+        ),
+        if (alphabet.isNotEmpty)
+          Padding(
+            padding: const EdgeInsets.only(top: 2),
+            child: Text(
+              alphabet,
+              style: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.w800,
+                letterSpacing: 2,
+                height: 1.0,
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
